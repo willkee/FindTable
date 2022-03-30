7902ac64d1151f18f8e58a6588f68a7c578365d2
Will Kee <william.w.kee@gmail.com>
2022-03-29 21:50:44 -0600

    modified gitignore to ignore DS_Store files, added updated schema

---
+++ app/models/cuisines.py
@@ -0,0 +1,14 @@
+# list_of_cuisines = [
+#     "American",
+#     "Mexican",
+#     "Asian",
+#     "European",
+#     "African",
+#     "Middle Eastern"
+#     ]
+
+# for cuisine in list_of_cuisines:
+#     new_cuisine = Cuisine(type=cuisine)
+#     db.session.add(new_cuisine)
+
+# db.session.commit()
