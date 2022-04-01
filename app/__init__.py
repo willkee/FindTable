2a2847e8c00c82efc093b9a5938c489e212cf61d
Will Kee <william.w.kee@gmail.com>
2022-04-01 16:13:45 -0600

    added starter file

--- app/__init__.py
+++ app/__init__.py
@@ -1,53 +1,72 @@
-from flask import Flask
-# import os
+import os
+from flask import Flask, render_template, request, session, redirect
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect, generate_csrf
+from flask_login import LoginManager
+
+from .models import db, User
+from .api.user_routes import user_routes
+from .api.auth_routes import auth_routes
+
+from .seeds import seed_commands
+
+from .config import Config
 
 app = Flask(__name__)
 
-import requests
-import json
-maps_api = os.environ.get('GOOGLE_MAPS_API')
-
-lower_manhattan = "40.7193%2C-74.0019"
-mid_manhattan = "40.7578%2C-73.9856"
-up_manhattan = "40.8234%2C-73.9453"
-brooklyn = "40.6947%2C-73.9845"
-queens = "40.7255%2C-73.7919"
-bronx = "40.8423%2C-73.8449"
-staten_island = "40.6303%2C-74.1057"
+# Setup login manager
+login = LoginManager(app)
+login.login_view = 'auth.unauthorized'
 
 
-for i in range(10):
-    response_API = requests.get(f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={mid_manhattan}&radius=1500&type=restaurant&key={maps_api}')
-    parse_json = response_API.json()
-    restaurants = parse_json['results']
-    restaurant_name = restaurants[i]['name']
-
-    place_search = requests.get(f'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?&input={restaurant_name}&inputtype=textquery&locationbias=circle%3A2000%40{mid_manhattan}&key={maps_api}')
-    new_id = place_search.json()['candidates'][0]['place_id']
-
-    # details = requests.get(f'https://maps.googleapis.com/maps/api/place/details/json?fields=name%2Cformatted_phone_number%2Cwebsite&place_id={new_id}&key={maps_api}')
-    photos = requests.get(f'https://maps.googleapis.com/maps/api/place/details/json?fields=photos&place_id={new_id}&key={maps_api}')
-
-    photo_ref = photos.json()['result']['photos'][0]
-    photo_url = requests.get(f'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference={photo_ref}&key={maps_api}').url
-    print(photo_ref)
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
-    # print(details.json())
+# Tell flask about our seed commands
+app.cli.add_command(seed_commands)
+
+app.config.from_object(Config)
+app.register_blueprint(user_routes, url_prefix='/api/users')
+app.register_blueprint(auth_routes, url_prefix='/api/auth')
+db.init_app(app)
+Migrate(app, db)
+
+# Application Security
+CORS(app)
 
 
-# restaurant_seed = {}
-# for i in range(0, 20):
+# Since we are deploying with Docker and Flask,
+# we won't be using a buildpack when we deploy to Heroku.
+# Therefore, we need to make sure that in production any
+# request made over http is redirected to https.
+# Well.........
+@app.before_request
+def https_redirect():
+    if os.environ.get('FLASK_ENV') == 'production':
+        if request.headers.get('X-Forwarded-Proto') == 'http':
+            url = request.url.replace('http://', 'https://', 1)
+            code = 301
+            return redirect(url, code=code)
 
-#     # print(restaurants[i]['types'])
 
-#     restaurant_seed[i] = {
-#         'name': restaurants[i]['name'],
-#         'street_address': restaurants[i]['vicinity'],
-#         'price_rating': restaurants[i]['price_level'],
-#         'img_url': photo_url,
-#         'borough': "Manhattan",
-#         'accessible': True
-#     }
+@app.after_request
+def inject_csrf_token(response):
+    response.set_cookie(
+        'csrf_token',
+        generate_csrf(),
+        secure=True if os.environ.get('FLASK_ENV') == 'production' else False,
+        samesite='Strict' if os.environ.get(
+            'FLASK_ENV') == 'production' else None,
+        httponly=True)
+    return response
 
-# print(restaurant_seed)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def react_root(path):
+    if path == 'favicon.ico':
+        return app.send_static_file('favicon.ico')
+    return app.send_static_file('index.html')
