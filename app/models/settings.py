0e295b7e63db03c302e62ec05892a9179f360de7
Nikolas <nikolas.garza5@gmail.com>
2022-04-02 14:16:25 -0700

    restaurant model started. Still need relationships for reviews, reservations, favorites

---
+++ app/models/settings.py
@@ -0,0 +1,10 @@
+from .db import db
+import datetime
+
+class Setting(db.model):
+    __tablename__ = 'settings'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now()) # FORMAT: 2022-04-02 13:27:25.457314
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
