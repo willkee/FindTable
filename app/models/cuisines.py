9f6f5e9431f43f6f8bd077915a09d4f06b737012
Will Kee <william.w.kee@gmail.com>
2022-04-03 00:55:55 +0000

    added settings seed

--- app/models/cuisines.py
+++ app/models/cuisines.py
@@ -6,8 +6,8 @@ class Cuisine(db.Model):
     __tablename__ = 'cuisines'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    created_at = db.Column(db.DateTime, server_default=db.func.now()) # FORMAT: 2022-04-02 13:27:25.457314
-    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    type = db.Column(db.String(50), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.now()) # FORMAT: 2022-04-02 13:27:25.457314
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
 
     restaurants = db.relationship('Restaurant', secondary=restaurant_cuisines, back_populates="cuisines")
