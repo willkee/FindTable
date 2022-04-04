53f7682c502e3024b93a05348ed832e89bcc7917
Paul <dustin.paul.oh@gmail.com>
2022-04-04 11:11:51 -0700

    changed component structure

---
+++ react-app/src/components/Homepage/index.js
@@ -0,0 +1,14 @@
+import styles from './homepage.module.css'
+import NavBar from '../Navbar'
+
+const Homepage = () => {
+
+  return (
+    <div className={styles.wrapper}>
+      <NavBar />
+      <div className={styles.container}>
+
+      </div>
+    </div>
+  )
+}
