f8be60436176dbe7b7ec858f891eb5de78c8802b
Paul <dustin.paul.oh@gmail.com>
2022-04-03 17:07:31 -0700

    added restaurant_exists validator to name

--- app/forms/restaurant_form.py
+++ app/forms/restaurant_form.py
@@ -11,7 +11,7 @@ def restaurant_exists(form, field):
     raise ValidationError('Restaurant already exists.')
 
 class RestaurantForm(FlaskForm):
-  name = StringField('Name', validators=[DataRequired(), Length(min=0, max=255)])
+  name = StringField('Name', validators=[DataRequired(), Length(min=0, max=255), restaurant_exists])
   price_rating = IntegerField('Price Rating', validators=[DataRequired()])
   description = TextAreaField('Description')
   img_url = StringField('Image URL', validators=[DataRequired(), Length(min=0, max=2048)])
