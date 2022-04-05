7399ae2d9f90d670fe56b28c456d8c8b5473a09f
Paul <dustin.paul.oh@gmail.com>
2022-04-05 08:37:09 -0700

    fixed imports

--- react-app/src/components/Homepage/index.js
+++ react-app/src/components/Homepage/index.js
@@ -1,4 +1,4 @@
-import styles from './homepage.module.css'
+import styles from './Homepage.module.css'
 import {PageWrapper} from '../PageWrapper'
 import NavBar from '../Navbar'
 import {PageContainer} from '../PageContainer'
--- react-app/src/components/Navbar/index.js
+++ react-app/src/components/Navbar/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 import ProtectedRoute from '../auth/ProtectedRoute'
-import styles from './navbar.module.css'
+import styles from './Navbar.module.css'
 import {MagnifyingGlass, Calendar, UserProfileIcon, GreyVerticalLine} from '../Icons'
 
 
