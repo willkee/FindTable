e94b92cad2956ec069e56b2cd81ed93395804e13
Nikolas <nikolas.garza5@gmail.com>
2022-04-06 10:51:30 -0700

    moved restaraunt form

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -11,7 +11,7 @@ import User from './components/User'
 
 import UsersList from './components/UsersList';
 import RestaurantsList from './components/RestaurantsList';
-import NewRestaurantForm from './components/auth/NewRestaurantForm';
+import { NewRestaurant } from './Forms/RestaurantForm';
 import ProfilePage from './components/ProfilePage';
 
 import { receiveAllRestaurants } from './store/restaurants';
@@ -65,18 +65,18 @@ function App() {
             <Route path='/sign-up' exact={true}>
               <SignUpForm />
             </Route>
-            <Route exact path="/restaurants">
+            {/* <Route exact path="/restaurants">
               <Restaurants all_restaurants={all_restaurants} />
             </Route>
             <Route exact path="/restaurants/:id">
               <SingleRestaurant all_restaurants={all_restaurants} />
-            </Route>
+            </Route> */}
             <ProtectedRoute exact path="/restaurants/new">
               <NewRestaurant all_settings={all_settings} all_cuisines={all_cuisines}/>
             </ProtectedRoute>
-            <ProtectedRoute path='/reservations' exact={true} >
+            {/* <ProtectedRoute path='/reservations' exact={true} >
               <Reservations/>
-            </ProtectedRoute>
+            </ProtectedRoute> */}
             <ProtectedRoute path='/users' exact={true} >
               <UsersList users={users} />
               </ProtectedRoute>
@@ -86,9 +86,9 @@ function App() {
               <ProtectedRoute exact path="/restaurants">
               <RestaurantsList all_restaurants={all_restaurants} />
               </ProtectedRoute>
-              <ProtectedRoute exact path="/restaurants/new">
+              {/* <ProtectedRoute exact path="/restaurants/new">
                 <NewRestaurantForm all_settings={all_settings} all_cuisines={all_cuisines}/>
-              </ProtectedRoute>
+              </ProtectedRoute> */}
               <ProtectedRoute exact path="/my-profile">
                 <ProfilePage />
               </ProtectedRoute>
