32587f94b49d15a45776f1824d505465250d29da
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-04-06 10:41:51 -0600

    Added all icons

---
+++ react-app/src/Forms/RestaurantForm/index.js
@@ -0,0 +1,10 @@
+import NewRestaurantForm from "../../components/auth/NewRestaurantForm";
+import { PageContainer } from "../../components/PageContainer";
+
+export const NewRestaurant = () => {
+    return (
+        <PageContainer>
+            <NewRestaurantForm />
+        </PageContainer>
+    )
+}
