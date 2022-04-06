e101e6dbfac305fedcd7c74b8f9b10f57813a35a
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-04-06 12:07:39 -0600

    Reservations model and form is readey for review, but I need to change the Users model

--- app/models/reservations.py
+++ app/models/reservations.py
@@ -26,26 +26,19 @@ class Reservations(db.Model):
     created_at = db.Column(db.DateTime, default=db.func.now()) # FORMAT: 2022-04-02 13:27:25.457314
     updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
 
-    user_who_booked = db.relationship("User", back_populates="reservations")
     restaurants = db.relationship('Restaurant', secondary=restaurant_reservations, back_populates="reservations")
-    users = db.relationship('User', secondary=user_reservations, back_populates="reservations")
+    user_who_booked = db.relationship('User', secondary=user_reservations, back_populates="reservations")
 
     def to_dict(self):
         return {
             'id': self.id,
-            'owner_id': self.owner_id,
-            'name': self.name,
-            'price_rating': self.price_rating,
-            'description': self.description,
-            'img_url': self.img_url,
-            'phone_number': self.phone_number,
-            'website': self.website,
-            'street_address': self.street_address,
-            'borough': self.borough,
-            'accessible': self.accessible,
-            'settings': [setting.to_dict() for setting in self.settings],
-            'cuisines': [cuisine.to_dict() for cuisine in self.cuisines],
+            'restaurant_id': self.restaurant_id,
+            'user_id': self.user_id,
+            'num_people': self.num_people,
+            'date_time': self.date_time,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
         }
-# 'reviews': [review.to_dict() for review in self.reviews]
-# 'reservations': [reservation.to_dict() for reservation in self.reservations]
-# 'favorites': [favorite.to_dict() for favorite in self.favorites]
+
+    # I think adding created_at and updated_at to the dict() will be helpful
+    # We can do something like => new Date() - created_at === less than 24hours ? cannot update reservation : update resrvation
