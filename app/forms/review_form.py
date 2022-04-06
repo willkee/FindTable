8bab995fd3e0f0f005c58817aa5001e58eab197d
Paul <dustin.paul.oh@gmail.com>
2022-04-06 09:46:10 -0700

    reviewbackend branch created

--- app/forms/review_form.py
+++ app/forms/review_form.py
@@ -5,8 +5,9 @@ from wtforms.validators import DataRequired, Length
 
 class ReviewForm(FlaskForm):
     # Users can post one picture that is an image URL right now
-    # In the near future, this will be an AWS S3 URL 
+    # In the near future, this will be an AWS S3 URL
   stars = IntegerField('Name', validators=[DataRequired()])
   content = TextAreaField('Content')
   img_url = StringField('Image URL', validators=[DataRequired(), Length(min=0, max=2048)])
   submit = SubmitField('Submit')
+
