e4f36eabf6977e3b225a909f8f14f0cce096228f
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-04-06 11:32:22 -0600

    Added reservations WTForm

--- app/forms/reservation_form.py
+++ app/forms/reservation_form.py
@@ -6,6 +6,6 @@ from wtforms.validators import DataRequired, Length
 class ReservationForm(FlaskForm):
     # Users can post one picture that is an image URL right now
     # In the near future, this will be an AWS S3 URL
-  num_people = IntegerField('Name', validators=[DataRequired()])
+  num_people = Integ erField('Name', validators=[DataRequired()])
   date_time = DateTimeLocalField('Which date is your favorite?', format='%m/%d/%y', validators=[Required()])
   submit = SubmitField('Submit')
