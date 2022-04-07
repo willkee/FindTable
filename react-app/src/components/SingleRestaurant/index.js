c34837530b35bd4455eee77a7f29632401e0081f
Paul <dustin.paul.oh@gmail.com>
2022-04-07 11:14:04 -0700

    made changes to reviewform

--- react-app/src/components/SingleRestaurant/index.js
+++ react-app/src/components/SingleRestaurant/index.js
@@ -5,11 +5,13 @@ import { useParams } from 'react-router-dom';
 // import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 // import { createReview } from '../../store/reviews';
+import { ReviewForm } from '../../Forms/ReviewForm';
 
 export const SingleRestaurant = () => {
   const {id} = useParams()
   // const dispatch = useDispatch()
   const restaurant = useSelector(state => Object.values(state.restaurants))[id - 1]
+  // console.log(restaurant)
   // const handleNewReview = () => {
   //   dispatch(createReview({
 
@@ -24,6 +26,7 @@ export const SingleRestaurant = () => {
               <div>{restaurant.street_address}</div>
               <div>{restaurant.borough}</div>
               <div>{restaurant.website}</div>
+              <ReviewForm props={restaurant}/>
             </PageContainer>
         </PageWrapper>
     )
