301dc85a6827ada97f75f644cdecd92466a0f8c7
Will Kee <william.w.kee@gmail.com>
2022-04-07 18:03:46 +0000

    added sectioned off single restaurant page

--- react-app/src/components/SingleRestaurant/index.js
+++ react-app/src/components/SingleRestaurant/index.js
@@ -1,9 +1,10 @@
-// import styles from './SingleRestaurant.module.css';
+import styles from './SingleRestaurant.module.css';
 import { PageWrapper } from '../PageWrapper';
 import { PageContainer } from '../PageContainer';
 import { useParams } from 'react-router-dom';
 // import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import pattern from './pattern.png'
 // import { createReview } from '../../store/reviews';
 
 export const SingleRestaurant = () => {
@@ -18,12 +19,20 @@ export const SingleRestaurant = () => {
 
   return (
         <PageWrapper>
-            <PageContainer>
-              <div>{restaurant.name}</div>
-              <img src={restaurant.img_url} alt="" width="200px"></img>
-              <div>{restaurant.street_address}</div>
-              <div>{restaurant.borough}</div>
-              <div>{restaurant.website}</div>
+            <PageContainer className={styles.sr_custom_pc}>
+              <img src={pattern} className={styles.sr_banner} alt="banner pattern"></img>
+              <img className={styles.sr_img} src={restaurant.img_url} alt="" width="200px"></img>
+              <div className={styles.left_sidebar}></div>
+              <div className={styles.sr_parent}>
+
+                <div className={styles.sr_content}>
+                  <h2>{restaurant.name}</h2>
+                  <div>{restaurant.street_address}</div>
+                  <div>{restaurant.borough}</div>
+                  <div>{restaurant.website}</div>
+                </div>
+              </div>
+              <div className={styles.right_sidebar}></div>
             </PageContainer>
         </PageWrapper>
     )
