5db1d07841857660dd43176688f88a57477ffdad
Paul <dustin.paul.oh@gmail.com>
2022-04-07 15:20:37 -0700

    updated reservations models

--- app/models/reservations.py
+++ app/models/reservations.py
@@ -11,8 +11,8 @@ class Reservation(db.Model):
     created_at = db.Column(db.DateTime, default=db.func.now()) # FORMAT: 2022-04-02 13:27:25.457314
     updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
 
-    restaurants = db.relationship('Restaurant', secondary=restaurant_reservations, back_populates="reservations")
-    user_who_booked = db.relationship('User', secondary=user_reservations, back_populates="reservations")
+    restaurants = db.relationship('Restaurant', back_populates="reservations")
+    user_who_booked = db.relationship('User', back_populates="reservations")
 
     def to_dict(self):
         return {
