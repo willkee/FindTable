aea37a484df716c74a5361ba43b83ca49524c65d
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-04-08 16:16:08 -0600

    Fixing the gooter

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -13,7 +13,7 @@ import RestaurantsList from './components/RestaurantsList';
 import ProfilePage from './components/ProfilePage';
 import SearchResults from './components/SearchResults';
 import { Homepage } from './components/Homepage';
-// import { Footer } from "./components/Footer"
+import { Footer } from "./components/Footer"
 import { SingleRestaurant } from './components/SingleRestaurant';
 import { PageWrapper } from '../src/components/PageWrapper';
 import { CreateRestaurant } from './components/CreateRestaurant'
@@ -99,7 +99,7 @@ function App() {
               </Route>
             </Switch>
             <div className='return_to_top_button'><a href="#top"><i className="fa-solid fa-angles-up"></i></a></div>
-            {/* <Footer /> */}
+            <Footer />
       </PageWrapper>
     </BrowserRouter>
   );
