de8b99441143ea4c46295ec4a5a1f1b6e5f076bf
Paul <dustin.paul.oh@gmail.com>
2022-04-07 19:41:11 -0700

    fixed warnings

--- react-app/src/components/Homepage/index.js
+++ react-app/src/components/Homepage/index.js
@@ -4,7 +4,7 @@ import SearchHeader from '../SearchHeader'
 // import { useDispatch } from 'react-redux'
 // import { getLocation } from '../../store/session'
 
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 
 import { ReservationCard } from '../ReservationCard'
 
@@ -59,7 +59,7 @@ export const Homepage = () => {
 
       <SearchHeader className={styles.search_header_component} />
       {<div>
-        {userLocation}
+        {/* {userLocation} */}
       </div>}
       <ReservationCard />
 
