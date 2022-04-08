b04d26aeb5292d629baa1684f62064b8e53f31b5
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-04-08 16:23:33 -0600

    Test

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -8,7 +8,7 @@ export const Footer = () => {
         <footer className={styles.footer}>
             <div className={styles.container}>
                 <section className={styles.links}>
-                    <div className={styles.discover}>
+                    <div className={styles.linksBox}>
                         <ul className={styles.label}>DISCOVER
                             <li className={styles.sectionLinks}><Link className={styles.sectionLinks} to="/cuisines">Cuisines Near Me</Link></li>
                             <li className={styles.sectionLinks}><Link className={styles.sectionLinks} to="/restaurants">Restaurants Near Me</Link></li>
@@ -16,9 +16,9 @@ export const Footer = () => {
                             <li className={styles.sectionLinks}><Link className={styles.sectionLinks} to="/cuisines">Cuisines</Link></li>
                        </ul>
                     </div>
-                    <div className={styles.about}>
+                    <div className={styles.linksBox}>
                         <ul className={styles.label}>FindTable
-                        <li className={styles.sectionLinks}><a className={styles.sectionLinks} href="https://github.com/willkee/FindTable" rel="noreferrer" target="_blank">About Us</a></li>
+                            <li className={styles.sectionLinks}><a className={styles.sectionLinks} href="https://github.com/willkee/FindTable" rel="noreferrer" target="_blank">About Us</a></li>
                         </ul>
                     </div>
                 </section>
