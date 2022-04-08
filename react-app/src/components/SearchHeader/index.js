d26f72c4bb4257ae910a867285c2ec14dbca780d
Will Kee <william.w.kee@gmail.com>
2022-04-08 22:11:39 +0000

    fixed search results

--- react-app/src/components/SearchHeader/index.js
+++ react-app/src/components/SearchHeader/index.js
@@ -1,29 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import styles from './SearchHeader.module.css'
-import Calendar from 'react-calendar'
-// import Datetime from 'react-datetime'
-import { CalendarIcon } from '../Icons'
 
 const SearchHeader = () => {
-    const [date, setDate] = useState(new Date())
     const [searchQuery, setSearchQuery] = useState("")
-    const [showCalendar, setShowCalendar] = useState(false)
-    const [time, setTime] = useState("08:00")
-
-    const openCalendar = () => setShowCalendar(!showCalendar)
     const history = useHistory()
-
-    useEffect(() => {
-        if (!showCalendar) return;
-
-        const closeCalendar = () => setShowCalendar(false);
-        document.addEventListener("click", closeCalendar)
-
-        console.log('flag', document.getElementsByClassName("react-calendar__month-view__days")[0])
-
-        return () => document.removeEventListener("click", closeCalendar)
-    })
 
     const handleSubmit = e => {
         e.preventDefault()
@@ -31,22 +12,12 @@ const SearchHeader = () => {
         if (searchQuery.includes("%")) {
             alert(`Please do not use the "percent" symbol in your search query.`)
             setSearchQuery("")
-            return
-        }
-
-        // console.log(date.toDateString())
-        // console.log(time)
-
-        if (searchQuery) {
-            history.push({
-                pathname: `/search/${date.toDateString()}/${time}/${searchQuery}`,
-                })
-            return
+        } else if (searchQuery) {
+            history.push(`/search/${searchQuery}`)
         } else {
             alert(`Please enter a search query.`)
-            return
         }
-
+        return
     }
 
 
