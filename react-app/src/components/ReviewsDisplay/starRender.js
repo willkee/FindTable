4d90c54d2ae43d55310e73b98052ec839422e447
Paul <dustin.paul.oh@gmail.com>
2022-04-09 12:53:37 -0700

    changed borders for reviews

---
+++ react-app/src/components/ReviewsDisplay/starRender.js
@@ -0,0 +1,47 @@
+export const starRender = (stars) => {
+  if (stars === 1) {
+    return (
+      <>
+      <i className="fa-solid fa-star"></i>
+      </>
+    )
+  }
+  if (stars === 2) {
+    return (
+      <>
+      <i className="fa-solid fa-star"></i>
+      <i className="fa-solid fa-star"></i>
+      </>
+    )
+  }
+  if (stars === 3) {
+    return (
+      <>
+      <i className="fa-solid fa-star"></i>
+      <i className="fa-solid fa-star"></i>
+      <i className="fa-solid fa-star"></i>
+      </>
+    )
+  }
+  if (stars === 4) {
+    return (
+      <>
+      <i className="fa-solid fa-star"></i>
+      <i className="fa-solid fa-star"></i>
+      <i className="fa-solid fa-star"></i>
+      <i className="fa-solid fa-star"></i>
+      </>
+    )
+  }
+  if (stars === 5) {
+    return (
+      <>
+      <i className="fa-solid fa-star"></i>
+      <i className="fa-solid fa-star"></i>
+      <i className="fa-solid fa-star"></i>
+      <i className="fa-solid fa-star"></i>
+      <i className="fa-solid fa-star"></i>
+      </>
+    )
+  }
+}
