109b6b6cbff39de4bdaf912a9558722eca6b676a
Will Kee <william.w.kee@gmail.com>
2022-04-10 04:55:18 +0000

    single restaurant form, reviews css redone

--- react-app/src/Forms/ReviewForm/index.js
+++ react-app/src/Forms/ReviewForm/index.js
@@ -1,75 +1,86 @@
-import { useState } from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import styles from "./ReviewForm.module.css"
-import { createReview } from '../../store/restaurants';
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import styles from "./ReviewForm.module.css";
+import { createReview } from "../../store/restaurants";
 
+export const ReviewForm = ({ restaurant }) => {
+	const [rating, setRating] = useState(1);
+	const [content, setContent] = useState("");
+	const [imgURL, setImgURL] = useState("");
+	const [errors, setErrors] = useState([]);
+	// const [star, setStar] = useState(<OutlineGreyStar />)
+	const user = useSelector((state) => state.session.user);
+	const dispatch = useDispatch();
 
-export const ReviewForm = ({restaurant}) => {
-    const [rating, setRating] = useState(1);
-    const [content, setContent] = useState("");
-    const [imgURL, setImgURL] = useState("");
-    const [errors, setErrors] = useState([])
-    // const [star, setStar] = useState(<OutlineGreyStar />)
-    const user = useSelector(state => state.session.user)
-    const dispatch = useDispatch()
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const formData = {
+			user_id: user.id,
+			restaurant_id: restaurant.id,
+			stars: rating,
+			img_url: imgURL,
+			review: content,
+		};
+		const data = await dispatch(createReview(formData));
+		if (data) return setErrors(data);
+	};
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const formData = {
-          user_id: user.id,
-          restaurant_id: restaurant.id,
-          stars: rating,
-          img_url: imgURL,
-          review: content
-        }
-        const data = await dispatch(createReview(formData));
-        if (data) return setErrors(data)
-    }
+	const handleReset = (e) => {
+		e.preventDefault();
+		setRating(1);
+		setContent("");
+		setImgURL("");
+	};
 
-    const handleReset = (e) => {
-      e.preventDefault()
-      setRating(1)
-      setContent("")
-      setImgURL("")
-    }
+	console.log(rating);
 
-    console.log(rating)
+	return (
+		<div className={styles.review_form_parent}>
+			<div className={styles.reviewHeader}>
+				<div>
+					<strong>
+						Hi {user?.first_name}, how was your experience?
+					</strong>
+				</div>
+				{errors.length > 0 && (
+					<div className={styles.error_container}>
+						{errors.map((error) => (
+							<div key={error}>{error.split(":")[1]}</div>
+						))}
+					</div>
+				)}
+			</div>
+			<form className={styles.reviewContainer}>
+				<div className={styles.sub1}>
+					{imgURL ? (
+						<img
+							src={imgURL}
+							alt=""
+							height="190px"
+							width="190px"
+							className={styles.image}
+						/>
+					) : (
+						<strong>Image Preview</strong>
+					)}
+				</div>
 
-    return (
-      <div className={styles.review_form_parent}>
-        <div className={styles.reviewHeader}>
-          <div>
-              <strong>Hi {user?.first_name}, how was your experience?</strong>
-          </div>
-          {errors.length > 0 &&
-            <div className={styles.error_container}>{errors.map(error => <div key={error}>{error.split(":")[1]}</div>)}</div>
-            }
-        </div>
-        <div>
-          <form className={styles.reviewContainer}>
-            <div className={styles.div1}>
-              {imgURL? <img src={imgURL} alt="" height="190px" width="190px" className={styles.image}/> : null}
-            </div>
-
-            <div className={styles.div2}>
-              <strong>Image Preview</strong>
-            </div>
-
-            <div className={styles.div3}>
-              <strong>Rate your experience</strong>
-            </div>
-
-            <div className={styles.div4}>
-              <select value={rating}
-                      onChange={(e) => setRating(e.target.value)}
-                      required>
-                <option value={1}>⭐️</option>
-                <option value={2}>⭐️⭐️</option>
-                <option value={3}>⭐️⭐️⭐️</option>
-                <option value={4}>⭐️⭐️⭐️⭐️</option>
-                <option value={5}>⭐️⭐️⭐️⭐️⭐️</option>
-              </select>
-              {/* <div className={styles.starsContainer}>
+				<div className={styles.sub2}>
+					<div className={styles.rating_container}>
+						<strong>Rate your experience</strong>
+						<div>
+							<select
+								value={rating}
+								onChange={(e) => setRating(e.target.value)}
+								required
+							>
+								<option value={1}>⭐️</option>
+								<option value={2}>⭐️⭐️</option>
+								<option value={3}>⭐️⭐️⭐️</option>
+								<option value={4}>⭐️⭐️⭐️⭐️</option>
+								<option value={5}>⭐️⭐️⭐️⭐️⭐️</option>
+							</select>
+							{/* <div className={styles.starsContainer}>
                   <input type="checkbox" id="star5" value={5} onClick={e => setRating(e.target.value)}/>
                   <label for="star5"></label>
 
@@ -85,41 +96,49 @@ export const ReviewForm = ({restaurant}) => {
                   <input type="checkbox" id="star1" value={1} onClick={e => setRating(e.target.value)}/>
                   <label for="star1"></label>
               </div> */}
-            </div>
+						</div>
+					</div>
 
-            <div className={styles.div5}>
-              <textarea className={styles.content}
-                          name="review"
-                          value={content}
-                          onChange={e => setContent(e.target.value)}
-                          required
-                          placeholder="Tell us how it was!">
-              </textarea>
-            </div>
+					<div className={styles.textbox}>
+						<textarea
+							className={styles.content}
+							name="review"
+							value={content}
+							onChange={(e) => setContent(e.target.value)}
+							required
+							placeholder="Tell us how it was!"
+						></textarea>
+					</div>
 
-            <div className={styles.div6}>
-              <input type="url"
-                      placeholder="Enter your image URL"
-                      onChange={e => setImgURL(e.target.value)}
-                      value={imgURL}
-                      className={styles.url}>
-              </input>
-            </div>
+					<div>
+						<input
+							type="url"
+							placeholder="Enter your image URL"
+							onChange={(e) => setImgURL(e.target.value)}
+							value={imgURL}
+							className={styles.url}
+						></input>
+					</div>
 
-            <div className={styles.div7}>
-              <div className={styles.reset} role='button' onClick={handleReset}>
-                Reset
-              </div>
-            </div>
-
-            <div className={styles.div8}>
-              <div className={styles.submit} role='button' onClick={handleSubmit} disabled={!rating || !content ? true : false}>
-                Submit
-              </div>
-            </div>
-
-          </form>
-        </div>
-      </div>
-    )
-}
+					<div className={styles.reset_submit}>
+						<div
+							className={styles.reset}
+							role="button"
+							onClick={handleReset}
+						>
+							Reset
+						</div>
+						<div
+							className={styles.submit}
+							role="button"
+							onClick={handleSubmit}
+							disabled={!rating || !content ? true : false}
+						>
+							Submit
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>
+	);
+};
--- react-app/src/components/ReviewsDisplay/index.js
+++ react-app/src/components/ReviewsDisplay/index.js
@@ -1,68 +1,91 @@
-import React from 'react'
-import styles from './ReviewsDisplay.module.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { ReviewIcon } from '../Icons';
-import {ReviewEdit} from '../ReviewEdit';
-import { deleteReview } from '../../store/restaurants';
-import { starRender } from './starRender';
+import React from "react";
+import styles from "./ReviewsDisplay.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { ReviewIcon } from "../Icons";
+import { ReviewEdit } from "../ReviewEdit";
+import { deleteReview } from "../../store/restaurants";
+import { starRender } from "./starRender";
 
+export const ReviewsDisplay = ({ restaurant }) => {
+	const allUsers = useSelector((state) => state?.users);
+	const reviews = Object.values(restaurant?.reviews);
+	const sessionUser = useSelector((state) => state.session.user);
+	const dispatch = useDispatch();
 
+	function getRandomInt(max) {
+		return Math.floor(Math.random() * max);
+	}
 
-export const ReviewsDisplay = ({restaurant}) => {
-  const allUsers = useSelector(state => state?.users)
-  const reviews = Object.values(restaurant?.reviews)
-  const sessionUser = useSelector(state => state.session.user)
-  const dispatch = useDispatch();
+	const handleDelete = (id) => {
+		dispatch(deleteReview(id));
+	};
 
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
+	const colors = ["#7D2C3B", "#983937", "#4E3C24", "#7E4675", "#24599D"];
 
-  const handleDelete = (id) => {
-    dispatch(deleteReview(id))
-  }
+	return (
+		<div className={styles.container}>
+			{reviews.map((review) => (
+				<div className={styles.singleReview}>
+					<div className={styles.r_left}>
+						<div className={styles.iconContainer}>
+							<div
+								className={styles.icon}
+								style={{
+									backgroundColor: `${
+										colors[getRandomInt(5)]
+									}`,
+								}}
+							>
+								{allUsers[`${review.user_id}`].first_name[0]}
+								{allUsers[`${review.user_id}`].last_name[0]}
+							</div>
+						</div>
 
-  const colors = [
-    "#7D2C3B",
-    "#983937",
-    "#4E3C24",
-    "#7E4675",
-    "#24599D"
-  ]
+						<div className={styles.nameAndCount}>
+							{review.user_first_name}
+							<br />
+							<div className={styles.reviewCount}>
+								{<ReviewIcon />}
+								{
+									Object.values(
+										allUsers[`${review.user_id}`].reviews
+									).length
+								}{" "}
+								reviews
+							</div>
+						</div>
+					</div>
 
-  return (
-      <div className={styles.container}>
-        {reviews.map((review) => (
-          <div className={styles.singleReview}>
-            <div className={styles.iconContainer}>
-              <div className={styles.icon} style={{backgroundColor:`${colors[getRandomInt(5)]}`}}>
-                {allUsers[`${review.user_id}`].first_name[0]}{allUsers[`${review.user_id}`].last_name[0]}
-              </div>
-            </div>
-            <div className={styles.nameAndCount}>
-              {review.user_first_name}<br />
-              <div className={styles.reviewCount}>
-              {<ReviewIcon/>}
-              {Object.values(allUsers[`${review.user_id}`].reviews).length} reviews
-              </div>
-            </div>
-            <div className={styles.image}>
-              <img src={review.img_url} alt='' className={styles.image}/>
-            </div>
-            <div className={styles.stars}>
-              {starRender(review.stars)}
-            </div>
-            <div className={styles.content}>
-              {review.review}
-            </div>
-            <div className={styles.editDelete}>
-              {sessionUser && review.user_id === sessionUser.id ?
-                <ReviewEdit review={review}/> : null
-              }
-              {sessionUser && review.user_id === sessionUser.id ?
-                <div className={styles.delete} onClick={handleDelete(review.id)}>Delete</div> : null
-              }
-            </div>
-          </div>))}
-      </div>
-)}
+					<div className={styles.image}>
+						<img
+							src={review.img_url}
+							alt=""
+							className={styles.image}
+						/>
+					</div>
+
+					<div className={styles.r_right}>
+						<div className={styles.stars}>
+							{starRender(review.stars)}
+						</div>
+						<div className={styles.content}>{review.review}</div>
+					</div>
+
+					<div className={styles.editDelete}>
+						{sessionUser && review.user_id === sessionUser.id ? (
+							<ReviewEdit review={review} />
+						) : null}
+						{sessionUser && review.user_id === sessionUser.id ? (
+							<div
+								className={styles.delete}
+								onClick={handleDelete(review.id)}
+							>
+								Delete
+							</div>
+						) : null}
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
