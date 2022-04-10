17fa8f168bb52065e00841492f8938d0e66ee905
Will Kee <william.w.kee@gmail.com>
2022-04-10 05:09:01 +0000

    single restaurant page fully styled

--- react-app/src/Forms/ReviewForm/index.js
+++ react-app/src/Forms/ReviewForm/index.js
@@ -61,7 +61,10 @@ export const ReviewForm = ({ restaurant }) => {
 							className={styles.image}
 						/>
 					) : (
-						<strong>Image Preview</strong>
+						<img className={styles.img_placeholder}
+							src="https://customercare.igloosoftware.com/.api2/api/v1/communities/10068556/previews/thumbnails/4fc20722-5368-e911-80d5-b82a72db46f2?width=680&height=680&crop=False"
+							alt="Preview"
+						/>
 					)}
 				</div>
 
