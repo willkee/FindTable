2a7ec86d48dcdca93f4c3ff016663dee3e473670
Will Kee <william.w.kee@gmail.com>
2022-04-10 19:33:48 +0000

    added error page, fixed search results page

--- react-app/src/components/PageContainer/index.js
+++ react-app/src/components/PageContainer/index.js
@@ -1,13 +1,14 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const PageContainer = styled.div`
-    width: 85vw;
-    height: auto;
-    display: flex;
-    flex-direction: column;
-    top: 55px;
-    align-items: center;
-    background-color: white;
-    padding-bottom: 65px;
-    filter: drop-shadow(0 3px 6px rgba(0, 0, 0, 0.16))
+	width: 85vw;
+	min-height: 750px;
+	height: auto;
+	display: flex;
+	flex-direction: column;
+	top: 55px;
+	align-items: center;
+	background-color: white;
+	padding-bottom: 65px;
+	filter: drop-shadow(0 3px 6px rgba(0, 0, 0, 0.16));
 `;
