0eec2335dbbd5d7aae6fec2e2d7a8933ddae9bd9
Will Kee <william.w.kee@gmail.com>
2022-04-11 02:40:48 +0000

    fixed bug with no validation on passwords not matching

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, Email, ValidationError, EqualTo
 from app.models import User
 
 
@@ -16,4 +16,5 @@ class SignUpForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), user_exists, Email()])
-    password = StringField('password', validators=[DataRequired()])
+    password = StringField('password', validators=[DataRequired(), EqualTo('confirmPassword', message="Passwords do not match.")])
+    confirmPassword = StringField('confirmPassword', validators=[DataRequired()])
