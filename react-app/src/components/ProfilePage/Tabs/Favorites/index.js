5a98c9ba25fc8f78d3d6c6dcf6371cf70c40d951
Paul <dustin.paul.oh@gmail.com>
2022-04-10 19:34:26 -0700

    added some code to favorites

--- react-app/src/components/ProfilePage/Tabs/Favorites/index.js
+++ react-app/src/components/ProfilePage/Tabs/Favorites/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import styles from './Favorites.module.css';
 
 export const Favorites = () => {
-	const myFavorites = useSelector(state => state.session.user.favorites)
+	const myFavorites = useSelector(state => Object.values(state.session.user.favorites).map((restaurant) => (restaurant.id)))
+  const restaurantState = useSelector(state => state.restaurants)
 
 	return (
     <>
       <h1>Your favorite restaurants</h1>
-      <div>
-        
+      <div className={styles.eachFavorite}>
+        {myFavorites.map((id) => (
+          <>
+          {restaurantState[`${id}`].name}
+          </>
+        ))}
       </div>
     </>
   )
